6273ceb6d6188be361cccc2ff2ab66df782b82e4
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-11 06:54:57 +0700

    menambahkan class KodeProduk

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+class KodeProduk
+{
+    string[] produk= { "Laptop", "Smartphone", "Tablet", "Headset", "Keyboard", "Mouse", "Printer", "Monitor", "Smartwatch", "Kamera" };
+    string[] kode= { "E100", "E101", "E102", "E103", "E104", "E105", "E106", "E107", "E108", "E109" };
+
+    public string getKode(string nmProduk)
+    {
+        for (int i = 0; i < produk.Length; i++) { 
+            if(produk[i] == nmProduk)
+            {
+                return kode[i];
+            }
+        }
+
+        return "";
+    }
+}
+
+class Run
+{
+    static void Main()
+    {
+        KodeProduk prod = new KodeProduk();
+        Console.Write("Masukkan nama produk: ");
+        string input = Console.ReadLine();
+        string kode = prod.getKode(input);
+        Console.WriteLine("Kode Produk: " + kode);
+    }
+}
